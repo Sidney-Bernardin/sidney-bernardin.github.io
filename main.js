05fcd38f64cd0a933e1f384342dc021e09e5b3d1
Sidney-Bernardin <sidney.bernardin@gmail.com>
2025-08-11 12:01:22 -0400

    Added cursor effect.

---
+++ main.js
@@ -0,0 +1,29 @@
+var cursor = document.createElement("div")
+cursor.classList.add("cursor")
+document.body.appendChild(cursor)
+
+var cursorBorder = document.createElement("div")
+cursorBorder.classList.add("cursor-border")
+document.body.appendChild(cursorBorder)
+
+if (cursor) {
+    window.addEventListener("mousemove", (e) => {
+        if (e.target.tagName !== "BUTTON") {
+
+            cursor.style = ""
+            cursor.style.transform = `translate(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%))`
+
+            cursorBorder.style = ""
+            cursorBorder.style.transform = `translate(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%))`
+
+            cursorBorder.classList.remove("on-focus")
+        } else {
+            cursor.style.background = "transparent"
+
+            cursorBorder.classList.add("on-focus")
+            cursorBorder.style.transform = `translate(${e.target.offsetParent.offsetLeft + e.target.offsetLeft}px, ${e.target.offsetParent.offsetTop + e.target.offsetTop}px)`
+            cursorBorder.style.width = e.target.clientWidth + "px"
+            cursorBorder.style.height = e.target.clientHeight + "px"
+        }
+    })
+}
